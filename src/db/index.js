b38f84e5a0fb993be573999a7c1dbf70c41a5e19
Sahil M <sahilmachhi29@gmail.com>
2024-07-26 20:33:14 +0530

    commit

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB = async () => {
+    try {
+        const databaseInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(databaseInstance.connection.host)
+        return databaseInstance
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+}
